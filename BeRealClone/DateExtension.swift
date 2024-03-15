baf23772fef52fdc7f0eeae94f7542181c76117f
cebaBryan <133716455+cebaBryan@users.noreply.github.com>
2024-03-15 00:26:20 -0400

    Removing Files/Final Commit

--- BeRealClone/DateExtension.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension DateFormatter {
-    static var postFormatter: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .full
-        return formatter
-    }()
-}
